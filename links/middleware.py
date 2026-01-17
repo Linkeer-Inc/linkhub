20cb998a868b6dda5ffd919e2d06d6a76cdf1ce1
Felipe Monteiro <fm3209742@gmail.com>
2026-01-17 16:22:21 -0300

    fix: allow tenants on public schema

--- links/middleware.py
+++ links/middleware.py
@@ -1,9 +1,13 @@
 from django.http import Http404
 from django_tenants.middleware import TenantMainMiddleware
+from django_tenants.utils import get_public_schema_name
 
 class TenantMiddleware(TenantMainMiddleware):
     def get_tenant(self, domain_model, hostname):
         tenant = super().get_tenant(domain_model, hostname)
+
+        if tenant.schema_name == get_public_schema_name():
+            return tenant
 
         if not tenant.is_active:
             raise Http404('tenant not active')
