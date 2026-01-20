8470da7f5a980e53eb7dd1bdcf7cdffbe20ee612
Felipe Monteiro <fm3209742@gmail.com>
2026-01-19 22:41:09 -0300

    ref: better links page ui

--- linkhub/context_processors.py
+++ linkhub/context_processors.py
@@ -1,4 +1,10 @@
+from django.http.request import split_domain_port
+
 def tenant(request):
+    domainFromRequest, port = split_domain_port(request.get_host())
+    domain = request.tenant.domains.filter(domain=domainFromRequest).first()
+
     return {
         "tenant": getattr(request, "tenant", None),
+        "domain": domain
     }
