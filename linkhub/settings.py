0dcb4257a8279dcfbee1af418ea090b1e2c53e43
Felipe Monteiro <62250734+felipee-monteiro@users.noreply.github.com>
2026-09-09 20:26:39 -0300

    Simplify dotenv loading in settings.py
    Removed unnecessary environment loading check.

--- linkhub/settings.py
+++ linkhub/settings.py
@@ -16,10 +16,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-is_env_loaded = load_dotenv(BASE_DIR / '.env')
-
-if not is_env_loaded:
-    raise RuntimeError('.env file must exists')
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/dieployment/checklist/
